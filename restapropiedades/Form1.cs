206cae3ed1620843246b4c619bc1725c452208ea
Angel González <anngeeldavid@gmail.com>
2022-09-22 19:36:59 -0600

    Agregar archivos de proyecto.

---
+++ restapropiedades/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restapropiedades
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resta RESTA = new resta();
+            RESTA.V1 = double.Parse(textBox1.Text);
+            RESTA.V2 = double.Parse(textBox2.Text);
+
+            textBox3.Text = RESTA.restar().ToString();
+        }
+    }
+}
---
+++ restapropiedades/resta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restapropiedades
+{
+    class resta
+    {
+        private double v1;
+        private double v2;
+
+        public double V1 { get => v1; set => v1 = value; }
+        public double V2 { get => v2; set => v2 = value; }
+
+        public double restar()
+        {
+            return V1 - V2;
+        }
+    }
+}
